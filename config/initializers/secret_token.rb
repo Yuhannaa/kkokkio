09a63b3de27e23b3c22f3119c3ad5a7e33de1603
Yuhannaa <yuhannaa@gmail.com>
2013-04-24 22:57:57 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kkokkio::Application.config.secret_key_base = '221cdd7991706bff22103b4461e85afe41b931e0ec064fc2b5a8fe020b809baabe893e5593d82d1699f9616df2404051d5b639065d45b17a20b4bffb400dd801'
