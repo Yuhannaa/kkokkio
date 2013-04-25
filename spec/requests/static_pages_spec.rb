ceeec2d5e7278d5f3dc0f1e58fb427d250eb52bb
Yuhannaa <yuhannaa@gmail.com>
2013-04-25 12:20:01 +0900

    Add routes

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('KKoKKio') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('|') }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact'))}
+  end
+
+end
