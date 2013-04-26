39b6f642ebf5ee4a528e89b66f278029787f2555
Yuhannaa <yuhannaa@gmail.com>
2013-04-26 10:46:43 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "StaticPages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('KKoKKio') }
+    it { should have_content('KkokKio') }
     it { should have_title(full_title('')) }
     it { should_not have_title('|') }
   end
