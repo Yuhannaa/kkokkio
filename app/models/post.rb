ac90ac1e20a9b7eb9d5592ef466be223bb8183af
Yuhannaa <yuhannaa@gmail.com>
2013-05-02 01:25:43 +0900

    Add basic file(photo) attachment feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates :user_id, presence: true
   validates :content, presence: true
 end
