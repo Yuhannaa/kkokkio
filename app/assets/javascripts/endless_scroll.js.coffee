3c74c21d9429a029b74d42738825811cb6216cc6
Yuhannaa <yuhannaa@gmail.com>
2013-05-04 00:39:19 +0900

    Modify layout & UI

--- app/assets/javascripts/endless_scroll.js.coffee
+++ app/assets/javascripts/endless_scroll.js.coffee
@@ -2,7 +2,7 @@ jQuery ->
   if $('.pagination').length
     $(window).scroll ->
       url = $('.pagination .next_page a').attr('href')
-      if url && $(window).scrollTop() > $(document).height() - $(window).height() - 50
-        $('.pagination').text("Fetching more users...")
+      if url && $(window).scrollTop() > $(document).height() - $(window).height() - 150
+        $('.pagination').html("<p><i class='icon-time'></i> 불러오는 중..</p>")
         $.getScript(url)
     $(window).scroll
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.paginate(page: params[:page])
+    @posts = @user.posts.paginate(page: params[:page], per_page: 10)
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.paginate(page: params[:page], per_page: 20)
   end
 
   def create
@@ -50,14 +50,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user = User.find(params[:id])
-    @users = @user.followed_users.paginate(page: params[:page])
+    @users = @user.followed_users.paginate(page: params[:page], per_page: 20)
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: params[:page], per_page: 20)
     render 'show_follow'
   end
 
